cd3be271a85675fc9ee9131c21665d43e6d2bd73
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-04-23 18:46:49 -0600

    feat: Create project and update dependencies from template creation

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.pokeflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:pokeflutter/app/app.dart';
+import 'package:pokeflutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
